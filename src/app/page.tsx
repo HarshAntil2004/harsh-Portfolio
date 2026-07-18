bb0e86a53353571c8f370e158c408050c8037373
HarshAntil2004 <antilharsh1981@gmail.com>
2026-07-18 21:00:49 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Skills } from "@/components/Skills";
 import { Experience } from "@/components/Experience";
 import { Projects } from "@/components/Projects";
 import { Credentials } from "@/components/Credentials";
-import { Testimonials } from "@/components/Testimonials";
 import { Blog } from "@/components/Blog";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
@@ -25,7 +24,6 @@ export default function Home() {
         <Experience />
         <Skills />
         <Credentials />
-        <Testimonials />
         <Blog />
         <Contact />
       </main>
